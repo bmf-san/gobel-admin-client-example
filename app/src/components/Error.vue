26498578a027719c563be4d7811fb5a30b5c1eb5
bmf-san <bmf.infomation@gmail.com>
2020-03-17 19:57:16 +0900

    [update] 0.0.1

---
+++ app/src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error" v-if="error">
+    <p>{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error",
+  props: {
+    error: String
+  }
+};
+</script>
+
+<style scoped>
+.error p {
+  color: red;
+}
+</style>
