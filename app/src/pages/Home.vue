805af29687bfc5ebd3e5d8e81c3edbe72b9ef01a
bmf-san <bmf.infomation@gmail.com>
2020-08-05 02:06:47 +0900

    tmp

--- app/src/pages/Home.vue
+++ app/src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <span></span>
   </div>
 </template>
 
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,6 +4,7 @@ import Signin from "../pages/Signin.vue";
 import Home from "../pages/Home.vue";
 import Posts from "../pages/Posts.vue";
 import EditPost from "../pages/EditPost.vue";
+import apiClient from "../modules/apiClient";
 
 Vue.use(VueRouter);
 
@@ -47,15 +48,22 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    // TODO: auth checkapiを用意する
-    const isAuthenticated = true
-    if (!isAuthenticated) {
-      next({
-        path: '/signin',
+    apiClient
+      .get("/private/me", {
+        headers: {
+          'Authorization': "Bearer " + localStorage.getItem("access_token"),
+        }
       })
-    } else {
-      next()
-    }
+      .then(function() {
+        // TODO: Get admin data from response and set it to state or something for output admin data to views. Maybe this case needs store pattern.
+        next();
+      })
+      .catch(function(error) {
+        console.log(error);
+        next({
+          path: "/signin",
+        });
+      });
   } else {
     next()
   }
