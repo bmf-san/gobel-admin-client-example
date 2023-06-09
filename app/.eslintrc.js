2ca3845064de32c6474065f502723d7fd5d00c18
bmf-san <bmf.infomation@gmail.com>
2023-06-09 18:45:25 +0900

    [update] eslint

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -14,7 +14,9 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-v-model-argument": "off"
+    "vue/no-v-model-argument": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-reserved-component-names": "off"
   },
   overrides: [
     {
