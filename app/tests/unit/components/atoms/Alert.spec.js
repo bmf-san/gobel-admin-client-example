8500402408ed228e37cad877415ab5da89310da8
bmf-san <bmf.infomation@gmail.com>
2023-06-09 15:14:14 +0900

    [update] marked

--- app/tests/unit/components/atoms/Alert.spec.js
+++ app/tests/unit/components/atoms/Alert.spec.js
@@ -1,13 +1,13 @@
 import { shallowMount } from "@vue/test-utils";
 import Alert from "@/components/atoms/Alert";
 
-describe('Alert', () => {
-	let wrapper;
-	beforeEach(() => {
-		wrapper = shallowMount(Alert)
-	})
+describe("Alert", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Alert);
+  });
 
-	it('matches snapshot', () => {
-		expect(wrapper.html()).toMatchSnapshot();
-	})
-})
+  it("matches snapshot", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
